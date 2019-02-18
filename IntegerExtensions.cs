ba604a88adb2a2086f48467b9891acbc1b5de9fd
avocadokek <47501889+avocadokek@users.noreply.github.com>
2019-02-18 09:26:57 +0200

    Add files via upload

--- IntegerExtensions.cs
+++ IntegerExtensions.cs
@@ -6,8 +6,28 @@ namespace PadawansTask3
     {
         public static int Gcd(int a, int b)
         {
-            // put your code here
+            while ((a != 0) && (b != 0))
+            {
+                if (a > b)
+                    a -= b;
+                else
+                {
+
+                    b -= a;
+                }
+            }
+
+            return Math.Max(a, b);
+
+            throw new ArgumentNullException();
+            throw new ArgumentException();
             throw new NotImplementedException();
+        }
+
+        static void Main()
+        {
+            var result = Gcd(9, 1);
+            Console.WriteLine(result);
         }
     }
 }
